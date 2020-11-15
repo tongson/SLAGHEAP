4133d021952114ba03c74c3c28228eff33ba7621
Eduardo Tongson <propolice@gmail.com>
2020-11-15 14:43:49 +0800

    Yet another way to assign \n\t to a variable.

--- shell:dash_demo.sh
+++ shell:dash_demo.sh
@@ -3,6 +3,9 @@
 export LC_ALL=C
 LFTAB="$(printf "\\n\\tx")"
 export IFS="${LFTAB%x}"
+XLFTAB="$(printf '%b_' '\n\t')"
+XLFTAB="${XLFTAB%_}"
+test "$IFS" = "$XLFTAB"
 unset PWD # the only environment variable set by default
 invalid_command 2>&- | { printf "testing -e\\n"; exit 0; }
 invalid_command 2>&- | ( printf "testing -e\\n"; exit 0; )
