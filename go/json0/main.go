becaa8b0bb1fd1795d57401a540dd0c8bb4385bb
Eduardo Tongson <propolice@gmail.com>
2024-04-08 01:09:45 +0800

    Demo getting MESSAGE from sudo journal entry.

---
+++ go/json0/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	lib "github.com/tongson/gl"
+	json "github.com/tidwall/gjson"
+)
+
+const cCURSOR= "/var/run/json0.cursor"
+
+func main() {
+	var journalctl lib.RunArgs
+        journalctl = lib.RunArgs{
+		Exe: "/usr/bin/journalctl",
+                Args: []string{
+                	"--output=json-seq",
+                        fmt.Sprintf("--cursor-file=%s", cCURSOR),
+                },
+        }
+	if ret, stdout, stderr, goerr := journalctl.Run(); !ret {
+		fmt.Fprintf(os.Stderr, "stdout: %s\nstderr: %s\nerror: %s\n", stdout, stderr, goerr)
+		os.Exit(1)
+        } else {
+		json.ForEachLine(stdout, func(line json.Result) bool {
+			id := json.Get(line.String(), "SYSLOG_IDENTIFIER")
+			if id.String() == "sudo" {
+				message := json.Get(line.String(), "MESSAGE")
+				fmt.Println(message)
+			}
+			return true
+		})
+	}
+}
