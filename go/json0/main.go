d96555d67f89c858a5d0134e6874b084b962c27b
Eduardo Tongson <propolice@gmail.com>
2024-04-08 01:10:14 +0800

    go fmt.

--- go/json0/main.go
+++ go/json0/main.go
@@ -2,26 +2,26 @@ package main
 
 import (
 	"fmt"
-	"os"
-	lib "github.com/tongson/gl"
 	json "github.com/tidwall/gjson"
+	lib "github.com/tongson/gl"
+	"os"
 )
 
-const cCURSOR= "/var/run/json0.cursor"
+const cCURSOR = "/var/run/json0.cursor"
 
 func main() {
 	var journalctl lib.RunArgs
-        journalctl = lib.RunArgs{
+	journalctl = lib.RunArgs{
 		Exe: "/usr/bin/journalctl",
-                Args: []string{
-                	"--output=json-seq",
-                        fmt.Sprintf("--cursor-file=%s", cCURSOR),
-                },
-        }
+		Args: []string{
+			"--output=json-seq",
+			fmt.Sprintf("--cursor-file=%s", cCURSOR),
+		},
+	}
 	if ret, stdout, stderr, goerr := journalctl.Run(); !ret {
 		fmt.Fprintf(os.Stderr, "stdout: %s\nstderr: %s\nerror: %s\n", stdout, stderr, goerr)
 		os.Exit(1)
-        } else {
+	} else {
 		json.ForEachLine(stdout, func(line json.Result) bool {
 			id := json.Get(line.String(), "SYSLOG_IDENTIFIER")
 			if id.String() == "sudo" {
